7daac19309bf948c4a2a7df7d93be3aadaee6b69
MinJunKimsdaads <aaeexx@naver.com>
2023-12-01 17:57:00 +0900

    1201

--- src/component/Test.tsx
+++ src/component/Test.tsx
@@ -4,22 +4,33 @@ import {useContext} from "react";
 function Test(){
     const dialog = useContext(DialogContext);
 
+    // const testFn = () => {
+    //     console.log('test');
+    // }
+
     const object ={
         title: "testTitle",
         des: "testDes",
-        styles: {
-            dialogShadow:'',
-            dialogColor:'',
-            dialogWhith:'',
-            dialogHeight:'',
-            dialogRadius:'',
-            dialogTitleFont:'',
-            dialogTitleColor:'',
-            dialogDesFont:'',
-        },
-        btn :{
-            
-        }
+        // styles: {
+        //     dialogShadow:'',
+        //     dialogColor:'',
+        //     dialogWhith:'',
+        //     dialogHeight:'',
+        //     dialogRadius:'',
+        //     dialogTitleFont:'',
+        //     dialogTitleColor:'',
+        //     dialogDesFont:'',
+        // },
+        // btn:{
+        //     btn1:{
+        //         name:'테스트',
+        //         action:testFn
+        //     },
+        //     btn2:{
+        //         name:'테스트2',
+        //         action:testFn
+        //     },
+        // }
     }
     
     return(
